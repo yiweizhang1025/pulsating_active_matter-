af523321016bcfdf8c204ea16c2f8fb1b17c14bc
Yiwei Zhang <yiweizhang1025@gmail.com>
2025-07-01 16:29:19 +0200

    Particles and Sites as Elements

---
+++ Elements/Sites.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+
+class Site {
+private:
+    int idX, idY; // Coordinates of the site in the grid
+    vector<int> particleIDs; 
+        vector<int> getParticleIDs(){
+        return particleIDs;
+    }
+public:
+    void popParticleID(int id){
+        for (int k = 0; k < this->particleIDs.size(); ++k){
+            if(particleIDs[k]==id){
+                particleIDs.erase(particleIDs.begin() + k);
+                break; // Exit after removing the first occurrence
+            }
+        }
+        exit("Error: particle ID not found in site");
+    }
+    /*
+    void copy_particle_ids_to_deque(deque<int> &d){
+        for(auto id : this->particle_ids){
+        d.push_back(id);
+        }
+    }
+    */
+    
+    void addParticleID(int id){
+        for (int k = 0; k < this->particleIDs.size(); ++k){
+            if(particleIDs[k]==id){
+                exit("Error: particle ID already exists in site");
+            }
+        }
+        this->particleIDs.push_back(id);
+    }
+}
